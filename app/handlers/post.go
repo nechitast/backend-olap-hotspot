0a0c644e56b30ff70eb0655d19c36977d6f8ff06
Nechita <nechitasamantha@gmail.com>
2025-07-01 17:17:31 +0700

    post.go and query.go change

--- app/handlers/post.go
+++ app/handlers/post.go
@@ -24,88 +24,201 @@ func AddHotspot(ctx *fiber.Ctx) error {
 }
 
 func GetHotspot(ctx *fiber.Ctx) error {
-	var hotspotsData []struct {
-		Hotspot_Count int     `gorm:"column:hotspot_count"`
-		Hotspot_Time  string  `gorm:"column:hotspot_time"`
-		Desa          string  `gorm:"column:desa"`
-		Kecamatan     string  `gorm:"column:kecamatan"`
-		Kab_kota      string  `gorm:"column:kab_kota"`
-		Provinsi      string  `gorm:"column:provinsi"`
-		Pulau         string  `gorm:"column:pulau"`
-		Longitude     float64 `gorm:"column:longitude"`
-		Latitude      float64 `gorm:"column:latitude"`
-		Confidence    string  `gorm:"column:confidence_level"`
-		Satelite      string  `gorm:"column:satelite_name"`
-		Time          string  `gorm:"column:id_time"`
-		Minggu        string  `gorm:"column:minggu"`
-	}
+    // Get query parameters for filtering
+    confidence := ctx.Query("confidence")
+    satelite := ctx.Query("satelite")
+    pulau := ctx.Query("pulau")
+    provinsi := ctx.Query("provinsi")
+    kota := ctx.Query("kota")
+    kecamatan := ctx.Query("kecamatan")
+    desa := ctx.Query("desa")
+    tahun := ctx.Query("tahun")
+    semester := ctx.Query("semester")
+    kuartal := ctx.Query("kuartal")
+    bulan := ctx.Query("bulan")
+    minggu := ctx.Query("minggu")
+    selectedDate := ctx.Query("selectedDate")
 
-	fmt.Println("Querying database for hotspots...")
+    var hotspotsData []struct {
+        Hotspot_Count int     `gorm:"column:hotspot_count"`
+        Hotspot_Time  string  `gorm:"column:hotspot_time"`
+        Desa          string  `gorm:"column:desa"`
+        Kecamatan     string  `gorm:"column:kecamatan"`
+        Kab_kota      string  `gorm:"column:kab_kota"`
+        Provinsi      string  `gorm:"column:provinsi"`
+        Pulau         string  `gorm:"column:pulau"`
+        Longitude     float64 `gorm:"column:longitude"`
+        Latitude      float64 `gorm:"column:latitude"`
+        Confidence    string  `gorm:"column:confidence_level"`
+        Satelite      string  `gorm:"column:satelite_name"`
+        Time          string  `gorm:"column:id_time"`
+        Minggu        string  `gorm:"column:minggu"`
+        Tahun         int     `gorm:"column:tahun"`
+        Semester      string  `gorm:"column:semester"`
+        Kuartal       string  `gorm:"column:kuartal"`
+        Bulan         string  `gorm:"column:bulan"`
+    }
 
-	err := clients.DATABASE.Raw(`
-		SELECT
-			fh.hotspot_count,
-			fh.hotspot_time::text,
-			dl.desa,
-			dl.kecamatan,
-			dl.kab_kota,
-			dl.provinsi,
-			dl.pulau,
-			ST_X(dl.geom_desa) AS longitude,
-			ST_Y(dl.geom_desa) AS latitude,
-			dc.confidence_level,
-			ds.satelite_name,
-			dt.id_time::text,
-			dt.minggu::text
-		FROM fact_hotspot fh
-		JOIN dim_location dl ON fh.id_location = dl.id_location
-		JOIN dim_confidence dc ON fh.id_confidence = dc.id_confidence
-		JOIN dim_satelite ds ON fh.id_satelite = ds.id_satelite
-		JOIN dim_time dt ON fh.id_time = dt.id_time
-	`).Scan(&hotspotsData).Error
+    fmt.Printf("Querying hotspots with selectedDate: %s\n", selectedDate)
 
-	if err != nil {
-		fmt.Println("Database error in GetHotspot:", err)
-		return ResponseJson(ctx, http.StatusInternalServerError, err.Error())
-	}
+    // Build dynamic query with WHERE conditions
+    query := `
+        SELECT
+            fh.hotspot_count,
+            fh.hotspot_time::text,
+            dl.desa,
+            dl.kecamatan,
+            dl.kab_kota,
+            dl.provinsi,
+            dl.pulau,
+            ST_X(dl.geom_desa) AS longitude,
+            ST_Y(dl.geom_desa) AS latitude,
+            dc.confidence_level,
+            ds.satelite_name,
+            dt.id_time::text,
+            dt.minggu::text,
+            dt.tahun,
+            dt.semester::text,
+            dt.kuartal::text,
+            dt.bulan::text
+        FROM fact_hotspot fh
+        JOIN dim_location dl ON fh.id_location = dl.id_location
+        JOIN dim_confidence dc ON fh.id_confidence = dc.id_confidence
+        JOIN dim_satelite ds ON fh.id_satelite = ds.id_satelite
+        JOIN dim_time dt ON fh.id_time = dt.id_time
+        WHERE 1=1`
 
-	fmt.Println("Hotspots retrieved:", len(hotspotsData))
-	if len(hotspotsData) == 0 {
-		fmt.Println("No data found in fact_hotspot for this query!")
-	}
+    var args []interface{}
+    argIndex := 1
 
-	// Format ke GeoJSON
-	geoJSON := fiber.Map{
-		"type":     "FeatureCollection",
-		"features": []fiber.Map{},
-	}
+    if selectedDate != "" {
+        query += fmt.Sprintf(" AND DATE(fh.hotspot_time) = $%d", argIndex)
+        args = append(args, selectedDate)
+        argIndex++
+        fmt.Printf("Applied date filter: %s\n", selectedDate)
+    }
 
-	for _, h := range hotspotsData {
-		geoJSON["features"] = append(geoJSON["features"].([]fiber.Map), fiber.Map{
-			"type": "Feature",
-			"properties": fiber.Map{
-				"confidence":    h.Confidence,
-				"satellite":     h.Satelite,
-				"time":          h.Time,
-				"minggu":		 h.Minggu,
-				"hotspot_count": h.Hotspot_Count,
-				"hotspot_time":  h.Hotspot_Time,
-				"location": fiber.Map{
-					"pulau":     h.Pulau,
-					"provinsi":  h.Provinsi,
-					"kab_kota":  h.Kab_kota,
-					"kecamatan": h.Kecamatan,
-					"desa":      h.Desa,
-				},
-			},
-			"geometry": fiber.Map{
-				"type":        "Point",
-				"coordinates": []float64{h.Longitude, h.Latitude},
-			},
-		})
-	}
+    if confidence != "" {
+        query += fmt.Sprintf(" AND dc.confidence_level = $%d", argIndex)
+        args = append(args, confidence)
+        argIndex++
+    }
+    
+    if satelite != "" {
+        query += fmt.Sprintf(" AND ds.satelite_name = $%d", argIndex)
+        args = append(args, satelite)
+        argIndex++
+    }
 
-	return ctx.JSON(geoJSON)
+    if pulau != "" {
+        query += fmt.Sprintf(" AND dl.pulau = $%d", argIndex)
+        args = append(args, pulau)
+        argIndex++
+    }
+
+    if provinsi != "" {
+        query += fmt.Sprintf(" AND dl.provinsi = $%d", argIndex)
+        args = append(args, provinsi)
+        argIndex++
+    }
+
+    if kota != "" {
+        query += fmt.Sprintf(" AND dl.kab_kota = $%d", argIndex)
+        args = append(args, kota)
+        argIndex++
+    }
+
+    if kecamatan != "" {
+        query += fmt.Sprintf(" AND dl.kecamatan = $%d", argIndex)
+        args = append(args, kecamatan)
+        argIndex++
+    }
+
+    if desa != "" {
+        query += fmt.Sprintf(" AND dl.desa = $%d", argIndex)
+        args = append(args, desa)
+        argIndex++
+    }
+
+    // Time filters
+    if tahun != "" {
+        query += fmt.Sprintf(" AND dt.tahun = $%d", argIndex)
+        args = append(args, tahun)
+        argIndex++
+    }
+
+    if semester != "" {
+        query += fmt.Sprintf(" AND dt.semester = $%d", argIndex)
+        args = append(args, semester)
+        argIndex++
+    }
+
+    if kuartal != "" {
+        query += fmt.Sprintf(" AND dt.kuartal = $%d", argIndex)
+        args = append(args, kuartal)
+        argIndex++
+    }
+
+    if bulan != "" {
+        query += fmt.Sprintf(" AND dt.bulan = $%d", argIndex)
+        args = append(args, bulan)
+        argIndex++
+    }
+
+    if minggu != "" {
+        query += fmt.Sprintf(" AND dt.minggu = $%d", argIndex)
+        args = append(args, minggu)
+        argIndex++
+    }
+
+    fmt.Printf("Final query: %s\n", query)
+    fmt.Printf("Query args: %v\n", args)
+
+    err := clients.DATABASE.Raw(query, args...).Scan(&hotspotsData).Error
+
+    if err != nil {
+        fmt.Printf("Database error in GetHotspot: %v\n", err)
+        return ResponseJson(ctx, http.StatusInternalServerError, err.Error())
+    }
+
+    fmt.Printf("Hotspots retrieved with filters: %d\n", len(hotspotsData))
+
+    // Format ke GeoJSON
+    geoJSON := fiber.Map{
+        "type":     "FeatureCollection",
+        "features": []fiber.Map{},
+    }
+
+    for _, h := range hotspotsData {
+        geoJSON["features"] = append(geoJSON["features"].([]fiber.Map), fiber.Map{
+            "type": "Feature",
+            "properties": fiber.Map{
+                "confidence":    h.Confidence,
+                "satellite":     h.Satelite,
+                "time":          h.Time,
+                "minggu":        h.Minggu,
+                "tahun":         h.Tahun,
+                "semester":      h.Semester,
+                "kuartal":       h.Kuartal,
+                "bulan":         h.Bulan,
+                "hotspot_count": h.Hotspot_Count,
+                "hotspot_time":  h.Hotspot_Time,
+                "location": fiber.Map{
+                    "pulau":     h.Pulau,
+                    "provinsi":  h.Provinsi,
+                    "kab_kota":  h.Kab_kota,
+                    "kecamatan": h.Kecamatan,
+                    "desa":      h.Desa,
+                },
+            },
+            "geometry": fiber.Map{
+                "type":        "Point",
+                "coordinates": []float64{h.Longitude, h.Latitude},
+            },
+        })
+    }
+
+    return ctx.JSON(geoJSON)
 }
 
 func AddLocation(ctx *fiber.Ctx) error {
@@ -210,4 +323,94 @@ func QueryLocation(ctx *fiber.Ctx) error {
 	}
 
 	return ResponseJson(ctx, http.StatusOK, formattedLocations)
+}
+
+func QueryLocationAggregated(ctx *fiber.Ctx) error {
+    // Get query parameters for filtering
+    selectedDate := ctx.Query("selectedDate")
+    confidence := ctx.Query("confidence")
+    satelite := ctx.Query("satelite")
+    pulau := ctx.Query("pulau")
+    provinsi := ctx.Query("provinsi")
+    kota := ctx.Query("kota")
+
+    fmt.Printf("QueryLocationAggregated with selectedDate: %s\n", selectedDate)
+
+    var locationData []struct {
+        Location string `gorm:"column:location"`
+        Total    int    `gorm:"column:total"`
+    }
+
+    // Build dynamic query for location aggregation with filters
+    query := `
+        SELECT 
+            dl.pulau as location,
+            COUNT(*) as total
+        FROM fact_hotspot fh
+        JOIN dim_location dl ON fh.id_location = dl.id_location
+        JOIN dim_confidence dc ON fh.id_confidence = dc.id_confidence
+        JOIN dim_satelite ds ON fh.id_satelite = ds.id_satelite
+        JOIN dim_time dt ON fh.id_time = dt.id_time
+        WHERE 1=1`
+
+    var args []interface{}
+    argIndex := 1
+
+    // Apply date filter
+    if selectedDate != "" {
+        query += fmt.Sprintf(" AND DATE(fh.hotspot_time) = $%d", argIndex)
+        args = append(args, selectedDate)
+        argIndex++
+    }
+
+    // Apply other filters
+    if confidence != "" {
+        query += fmt.Sprintf(" AND dc.confidence_level = $%d", argIndex)
+        args = append(args, confidence)
+        argIndex++
+    }
+    
+    if satelite != "" {
+        query += fmt.Sprintf(" AND ds.satelite_name = $%d", argIndex)
+        args = append(args, satelite)
+        argIndex++
+    }
+
+    if pulau != "" {
+        query += fmt.Sprintf(" AND dl.pulau = $%d", argIndex)
+        args = append(args, pulau)
+        argIndex++
+    }
+
+    if provinsi != "" {
+        query += fmt.Sprintf(" AND dl.provinsi = $%d", argIndex)
+        args = append(args, provinsi)
+        argIndex++
+    }
+
+    if kota != "" {
+        query += fmt.Sprintf(" AND dl.kab_kota = $%d", argIndex)
+        args = append(args, kota)
+        argIndex++
+    }
+
+    query += " GROUP BY dl.pulau ORDER BY total DESC"
+
+    fmt.Printf("Location aggregation query: %s\n", query)
+    fmt.Printf("Query args: %v\n", args)
+
+    err := clients.DATABASE.Raw(query, args...).Scan(&locationData).Error
+    if err != nil {
+        fmt.Printf("Database error in QueryLocationAggregated: %v\n", err)
+        return ResponseJson(ctx, http.StatusInternalServerError, err.Error())
+    }
+
+    // Format as [[location, total]]
+    result := make([][]interface{}, 0)
+    for _, item := range locationData {
+        result = append(result, []interface{}{item.Location, item.Total})
+    }
+
+    fmt.Printf("Location aggregation result: %v\n", result)
+    return ctx.JSON(result)
 }
--- app/handlers/query.go
+++ app/handlers/query.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nechitast/olap-backend/app/helpers"
 	"github.com/nechitast/olap-backend/app/models/payload"
 	"github.com/xdbsoft/olap"
+	"fmt"
 	
 )
 
@@ -33,6 +34,13 @@ func Query(ctx *fiber.Ctx) error {
 	if dimension == "time" {
 		return handleTimeQuery(ctx)
 	}
+
+	selectedDate := ctx.Query("selectedDate")
+    if selectedDate != "" && dimension == "location" {
+        fmt.Printf("Detected selectedDate filter: %s for dimension: %s\n", selectedDate, dimension)
+    	return handleLocationQueryWithDate(ctx, selectedDate)
+    }
+
 	point := ctx.Query("point")
 	dim := ctx.Query("dimension")
 	limit := ctx.QueryInt("limit", 100)
@@ -82,6 +90,131 @@ func Query(ctx *fiber.Ctx) error {
 	}
 
 	return ResponseJson(ctx, http.StatusOK, cube.Rows())
+}
+
+func handleLocationQueryWithDate(ctx *fiber.Ctx, selectedDate string) error {
+    // Get other filter parameters
+    confidence := ctx.Query("confidence")
+    satelite := ctx.Query("satelite")
+    pulau := ctx.Query("pulau")
+    provinsi := ctx.Query("provinsi")
+    kota := ctx.Query("kota")
+    kecamatan := ctx.Query("kecamatan")
+    desa := ctx.Query("desa")
+
+    fmt.Printf("handleLocationQueryWithDate - selectedDate: %s\n", selectedDate)
+    fmt.Printf("Other filters - confidence: %s, satelite: %s, pulau: %s, provinsi: %s, kota: %s\n", 
+        confidence, satelite, pulau, provinsi, kota)
+
+    var locationData []struct {
+        Location string `gorm:"column:location"`
+        Total    int    `gorm:"column:total"`
+    }
+
+    // Tentukan level agregasi berdasarkan parameter drill-down
+    var groupByColumn string = "dl.pulau" // Default ke pulau
+    
+    if desa != "" {
+        groupByColumn = "dl.desa"
+    } else if kecamatan != "" {
+        groupByColumn = "dl.desa" // Group by desa dalam kecamatan
+    } else if kota != "" {
+        groupByColumn = "dl.kecamatan" // Group by kecamatan dalam kota
+    } else if provinsi != "" {
+        groupByColumn = "dl.kab_kota" // Group by kota dalam provinsi  
+    } else if pulau != "" {
+        groupByColumn = "dl.provinsi" // Group by provinsi dalam pulau
+    }
+
+    // Build dynamic query
+    query := `
+        SELECT 
+            ` + groupByColumn + ` as location,
+            COUNT(*) as total
+        FROM fact_hotspot fh
+        JOIN dim_location dl ON fh.id_location = dl.id_location
+        JOIN dim_confidence dc ON fh.id_confidence = dc.id_confidence
+        JOIN dim_satelite ds ON fh.id_satelite = ds.id_satelite
+        JOIN dim_time dt ON fh.id_time = dt.id_time
+        WHERE 1=1`
+
+    var args []interface{}
+    argIndex := 1
+
+    // Apply date filter - INI YANG PENTING!
+    if selectedDate != "" {
+        query += ` AND DATE(fh.hotspot_time) = $` + fmt.Sprintf("%d", argIndex)
+        args = append(args, selectedDate)
+        argIndex++
+        fmt.Printf("Applied date filter to location query: %s\n", selectedDate)
+    }
+
+    // Apply other filters
+    if confidence != "" {
+        query += ` AND dc.confidence_level = $` + fmt.Sprintf("%d", argIndex)
+        args = append(args, confidence)
+        argIndex++
+    }
+    
+    if satelite != "" {
+        query += ` AND ds.satelite_name = $` + fmt.Sprintf("%d", argIndex)
+        args = append(args, satelite)
+        argIndex++
+    }
+
+    if pulau != "" {
+        query += ` AND dl.pulau = $` + fmt.Sprintf("%d", argIndex)
+        args = append(args, pulau)
+        argIndex++
+    }
+
+    if provinsi != "" {
+        query += ` AND dl.provinsi = $` + fmt.Sprintf("%d", argIndex)
+        args = append(args, provinsi)
+        argIndex++
+    }
+
+    if kota != "" {
+        query += ` AND dl.kab_kota = $` + fmt.Sprintf("%d", argIndex)
+        args = append(args, kota)
+        argIndex++
+    }
+
+    if kecamatan != "" {
+        query += ` AND dl.kecamatan = $` + fmt.Sprintf("%d", argIndex)
+        args = append(args, kecamatan)
+        argIndex++
+    }
+
+    if desa != "" {
+        query += ` AND dl.desa = $` + fmt.Sprintf("%d", argIndex)
+        args = append(args, desa)
+        argIndex++
+    }
+
+    // Group by location
+    query += ` GROUP BY ` + groupByColumn + ` ORDER BY total DESC`
+
+    fmt.Printf("Final location query: %s\n", query)
+    fmt.Printf("Query args: %v\n", args)
+
+    // Execute query
+    err := clients.DATABASE.Raw(query, args...).Scan(&locationData).Error
+    if err != nil {
+        fmt.Printf("Database error in handleLocationQueryWithDate: %v\n", err)
+        return ResponseJson(ctx, http.StatusInternalServerError, err.Error())
+    }
+
+    fmt.Printf("Location data retrieved with date filter: %d records\n", len(locationData))
+
+    // Format response as [[location, total]]
+    result := make([][]interface{}, 0)
+    for _, item := range locationData {
+        result = append(result, []interface{}{item.Location, item.Total})
+    }
+
+    fmt.Printf("Final result: %v\n", result)
+    return ResponseJson(ctx, http.StatusOK, result)
 }
 
 func handleTimeQuery(ctx *fiber.Ctx) error {
