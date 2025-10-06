17d099b7b18f570eba8cd16770de1fe2e1eb706a
Nechita <nechitasamantha@gmail.com>
2025-10-06 22:40:54 +0700

    change

--- app/handlers/post.go
+++ app/handlers/post.go
@@ -61,9 +61,6 @@ func GetHotspot(ctx *fiber.Ctx) error {
 
     fmt.Printf("Querying hotspots with selectedDate: %s\n", selectedDate)
 
-    limit := ctx.QueryInt("limit", 1000)
-    offset := ctx.QueryInt("offset", 0)
-    
     // Build dynamic query with WHERE conditions
     query := `
         SELECT
@@ -177,7 +174,6 @@ func GetHotspot(ctx *fiber.Ctx) error {
     fmt.Printf("Final query: %s\n", query)
     fmt.Printf("Query args: %v\n", args)
 
-    query += fmt.Sprintf(" LIMIT %d OFFSET %d", limit, offset)
     err := clients.DATABASE.Raw(query, args...).Scan(&hotspotsData).Error
 
     if err != nil {
